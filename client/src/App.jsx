f989b56382e22dd2b240e08ca9f689843d4f635b
samueljsong <semuall0108@gmail.com>
2023-07-13 13:01:04 -0700

    push

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Navbar } from './components/Navbar'
 import { BrowserRouter, Routes, Route } from "react-router-dom" 
 import { Store } from './pages/Store'
+import './fonts/roboto.ttf'
 
 
 function App() {
